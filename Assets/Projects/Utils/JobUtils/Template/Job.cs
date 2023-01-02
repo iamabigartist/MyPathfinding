b6e3b5c6806325c35291008bbf174780b2e24d3b
iamabigartist <pch4178@gmail.com>
2023-01-02 14:16:25 +0800

    add job template

---
+++ Assets/Projects/Utils/JobUtils/Template/Job.cs
@@ -0,0 +1,26 @@
+﻿using Unity.Burst;
+using Unity.Jobs;
+namespace Project.JobTerrainGen.Utils.JobUtil.Template
+{
+	[BurstCompile(
+		DisableSafetyChecks = true, OptimizeFor = OptimizeFor.Performance,
+		CompileSynchronously = true)]
+	public struct Job<TJobRunner> : IJob
+		where TJobRunner : struct, IJobRunner
+	{
+		public TJobRunner runner;
+		public void Execute()
+		{
+			runner.Execute();
+		}
+		public static void Plan(TJobRunner Runner, ref JobHandle deps)
+		{
+			var job = new Job<TJobRunner>() { runner = Runner };
+			deps = job.Schedule(deps);
+		}
+	}
+	public interface IJobRunner
+	{
+		void Execute();
+	}
+}
---
+++ Assets/Projects/Utils/JobUtils/Template/JobFor.cs
@@ -0,0 +1,36 @@
+﻿using Unity.Burst;
+using Unity.Jobs;
+namespace Project.JobTerrainGen.Utils.JobUtil.Template
+{
+	[BurstCompile(
+		DisableSafetyChecks = true, OptimizeFor = OptimizeFor.Performance,
+		CompileSynchronously = true)]
+	public struct JobFor<TJobForRunner> : IJobFor
+		where TJobForRunner : struct, IJobForRunner
+	{
+		public TJobForRunner runner;
+		public void Execute(int i)
+		{
+			runner.Execute(i);
+		}
+		public static void Plan(TJobForRunner Runner, ref JobHandle Deps)
+		{
+			var job = new JobFor<TJobForRunner>() { runner = Runner };
+			var (execute_len, inner_loop_batch_count) = Runner.ScheduleParam;
+			Deps = job.ScheduleParallel(execute_len, inner_loop_batch_count, Deps);
+		}
+
+		public static void PlanByRef(TJobForRunner Runner, ref JobHandle Deps, out JobFor<TJobForRunner> job)
+		{
+			job = new() { runner = Runner };
+			var (execute_len, inner_loop_batch_count) = Runner.ScheduleParam;
+			Deps = job.ScheduleParallelByRef(execute_len, inner_loop_batch_count, Deps);
+		}
+	}
+	public interface IJobForRunner
+	{
+		(int ExecuteLen, int InnerLoopBatchCount) ScheduleParam { get; }
+		void Execute(int i);
+
+	}
+}
